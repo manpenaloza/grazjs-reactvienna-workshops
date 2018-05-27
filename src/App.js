9e93febd024ea2c2f31409f9cc1cb7759fbeb859
Manuel Penaloza <manuel.penaloza@gmx.at>
2018-05-27 20:43:07 +0200

    further adjustments

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './App.css';
 import styled from 'styled-components';
 import { workshops } from "./content";
 
@@ -13,30 +12,80 @@ const MainWrapper = styled.div`
 `;
 
 const WrapperContentBox = styled.div`
-  flex: 1;
+  flex: 1.5;
   color: white;
-  border: 3px dotted #5F719F;
-  box-sizing: content-box;
 `;
 
 const WrapperFactsBox = styled(WrapperContentBox)`
-  border: 3px dotted #489851;
+  flex: 1;
+  background: #1D1F21;
 `
 
-const HeadlineWorkshops = styled.h2`
+const Headline = styled.h2`
   margin-top: 0;
   font-weight: normal;
   background-color: #5F719F;
+  padding: 1.5rem;
+  letter-spacing: .5vw;
 `;
 
-const HeadlineFacts = styled(HeadlineWorkshops)`
+const HeadlineFacts = styled(Headline)`
   background-color: #489851;
 `;
 
-const WorkshopBox = styled.div`
-  padding: 0 1em 2em 1em;
-  color: #333;
+const ContentBox = styled.div`
+  padding: 1.5rem;
   text-align: initial;
+`;
+
+const WorkshopsBox = styled(ContentBox)`
+
+`;
+
+const MainContainerWithColoredBackground = styled.div`
+  background: linear-gradient( to bottom, #489851 0%, #5F719F 100% ); 
+  min-height: 100vh;
+  padding: 1.7vw;
+  box-model: border-box;
+`;
+
+const ContentContainer = styled.div`
+  background: #1e2029;
+  height: 100%;
+  color: #ffffff;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  font-family: Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;
+  letter-spacing: 1.6px;
+`;
+
+const HeadlineContainer = styled.div`
+  border: 5px solid #2e313a;
+  font-size: 1.7vw;
+  padding: 1vw;
+  color: #fff;
+  text-align: center;
+`;
+
+const TutorLink = styled.a`
+  color: #5F719F;
+  font-size: 1.3em;
+`
+const CTAButton = styled.a`
+  display: inline-block;
+  margin-top: 1vw;
+  min-width: 12rem;
+  cursor: pointer;
+  font-size: 1.3em;
+  background: linear-gradient(90deg,#5F719F,#489851);
+  padding: 15px 18px;
+  border-radius: 5px;
+  text-decoration: none;
+  border: none;
+  color: #1D2029;
+  font-weight: 800;
+  text-align: center;
 `;
 
 const Workshop = ({
@@ -48,7 +97,14 @@ const Workshop = ({
   maxParticipants,
 }) => {
   return (
-    <div>{nameTutor}</div>
+    <section>
+      <h3>{title}</h3>
+      <p>with <TutorLink href="#">{nameTutor}</TutorLink></p>
+      <p>{description}</p>
+      <p style={{ fontWeight: 700, fontSize: "1.3em", }}>maximum participants: {maxParticipants}</p>
+      <CTAButton>Book a place</CTAButton>
+      <hr style={{ borderColor: "#1D1F21", marginBottom: "4vw", marginTop: "2vw" }}/>
+    </section>
   )
 }
 
@@ -56,24 +112,27 @@ class App extends Component {
   render() {
 
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">ReactVienna Crew Workshops @ Styria</h1>
-        </header>
-        <MainWrapper>
-          <WrapperContentBox>
-            <HeadlineWorkshops>workshops</HeadlineWorkshops>
-            <WorkshopBox>
-              {
-                Object.keys(workshops).map( workshopKey => <Workshop {...workshops[workshopKey]}/> )
-              }
-            </WorkshopBox>
-          </WrapperContentBox>
-          <WrapperFactsBox>
-            <HeadlineFacts>venue, date and time</HeadlineFacts>
-          </WrapperFactsBox>
-        </MainWrapper> 
-      </div>  
+      <MainContainerWithColoredBackground>
+        <ContentContainer>
+          <HeadlineContainer>
+            <h1>ReactVienna Crew Workshops @ Styria</h1>
+          </HeadlineContainer>
+          <MainWrapper>
+            <WrapperContentBox>
+              <Headline>workshops:</Headline>
+              <WorkshopsBox>
+                {
+                  Object.keys(workshops)
+                    .map( workshopKey => <Workshop {...workshops[workshopKey]}/> )
+                }
+              </WorkshopsBox>
+            </WrapperContentBox>
+            <WrapperFactsBox>
+              <HeadlineFacts>venue / date / time:</HeadlineFacts>
+            </WrapperFactsBox>
+          </MainWrapper> 
+        </ContentContainer>
+      </MainContainerWithColoredBackground>  
     );
   }
 }
--- src/content.js
+++ src/content.js
@@ -2,7 +2,7 @@ export const workshops = {
   patrick: {
     nameTutor: "Patrick Stapfer",
     title: "Getting Started with ReasonML & ReasonReact",
-    description: "tba",
+    description: "ReasonML is a new cool JavaScript-like language with a strong type-system and first level React bindings. As with every new language, there comes a new ecosystem and unfamiliar concepts with it.\n In this workshop, attendees will first of all learn how to setup and use ReasonML with React and find explanations on how to find relevant information for their coding problems (navigating the docs, leverage the community).\n Later own they will discover the capabilities of the underlying JS-compiler platform called BuckleScript and see how Reason translates to JavaScript. They slowly will get introduced to the interoperability layer to be able to interact with existing JavaScript code and learn how the React component model works in practise.",
     price: "Euro 290,00",
     workshops: [
       { 
@@ -18,7 +18,7 @@ export const workshops = {
   andrey: {
     nameTutor: "Andrey Okonetchnikov",
     title: "Component-driven Development",
-    description: "tba",
+    description: "Web applications becoming bigger and more complex, and as they grow, the complexity of the User Interface, arguably the most important part of your application, increases. And with it, your responsibility as a UI developer.\n A traditional development workflow — change code, refresh the app in the browser, click-click-click to reproduce the desired state, repeat — doesn't work anymore. Tools like React hot loader make it faster but don't really fix the underlying issue.",
     price: "Euro 290,00",
     workshops: [
       { 
@@ -33,8 +33,8 @@ export const workshops = {
   },
   Juho: {
     nameTutor: "Juho Vespäläinen",
-    title: "Component-driven Development",
-    description: "tba",
+    title: "Webpack - The Good Parts",
+    description: "In this high-level overview you'll learn how to configure Webpack. Even if you know it already, there might be some surprises in store as you gain insight to the tool you otherwise might miss.",
     price: "Euro 145,00",
     workshops: [
       { date: "06.06.2018", time: {
